a094427a4d710be987afca808b14e87c4280ef5c
huchangkuang <603021137@qq.com>
2020-07-28 16:47:43 +0800

    限制标签页新增标签输入框长度为4

--- src/components/tag/EditInput.vue
+++ src/components/tag/EditInput.vue
@@ -1,7 +1,7 @@
 <template>
     <label>
         <span>标签名</span>
-        <input type="text" :value="value" placeholder="输入类别名称，建议不超过四个汉字"
+        <input type="text" :value="value" placeholder="输入类别名称，建议不超过四个汉字" maxlength="4"
                @input="input($event.target.value)">
     </label>
 </template>
